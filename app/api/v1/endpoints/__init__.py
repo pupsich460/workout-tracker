8989f0f3fd4bd26eede574e635df0e643245d58a
pupsich <itschrizyman@yandex.ru>
2026-06-09 20:32:18 +0500

    fix endpoints

--- app/api/v1/endpoints/__init__.py
+++ app/api/v1/endpoints/__init__.py
@@ -1,5 +1,9 @@
+"""Импорт всех роутеров для автоматической регистрации в основном приложении."""
+
 from app.api.v1.endpoints.exercise import router as exercise_router  # noqa
 from app.api.v1.endpoints.user import router as user_router  # noqa
 from app.api.v1.endpoints.workout import router as workout_router  # noqa
 from app.api.v1.endpoints.workout_logs import router as workout_log_router  # noqa
-from app.api.v1.endpoints.workout_schedule import router as workout_schedule_router  # noqa
+from app.api.v1.endpoints.workout_schedule import (  # noqa
+    router as workout_schedule_router,
+)
