5b0c3ff96553ed6cdc8e7c49036e8d08d7589917
pupsich <itschrizyman@yandex.ru>
2026-06-09 14:02:05 +0500

    Update README

--- app/core/user.py
+++ app/core/user.py
@@ -17,11 +17,12 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.config import settings
 from app.core.db import get_async_session
-from app.core.logger import get_user_logger
+from app.core.logger import get_user_logger, setup_logger
 from app.models.user import User
 from app.schemas.user import UserCreate
 
-logger = get_user_logger(__name__)
+base_logger = setup_logger(__name__)
+logger = get_user_logger(base_logger)
 
 
 async def get_user_db(
